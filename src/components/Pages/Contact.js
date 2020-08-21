88ed2da8a1a4edcbe3df34f3c9841ecbcc657338
Andy <ajiajiechen@gmail.com>
2020-08-20 22:57:19 -0700

    added readable links to contact icons

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./style.css"
 
 const Contact = () =>{
     return(
@@ -9,9 +10,13 @@ const Contact = () =>{
                     <h1>Contact</h1>
                     <div class="row"><h6>Contact or Follow Me Here!</h6></div>
                     <div class="row">
-                        <a target="_blank" href="mailto:andychen24@hotmail.com"><i class="fa fa-envelope"></i></a>
-                        <a target="_blank" href="https://github.com/AndyJChen24"><i class="fa fa-github"></i></a>
-                        <a target="_blank" href="https://www.linkedin.com/in/andy-chen-7b52711a4/"><i class="fa fa-linkedin"></i></a>
+                        <a href="mailto:andychen24@hotmail.com"><i class="fa fa-envelope"></i> mailto:andychen24@hotmail.com</a>
+                    </div>
+                    <div class="row">
+                        <a target="_blank" href="https://github.com/AndyJChen24"><i class="fa fa-github"></i> https://github.com/AndyJChen24</a>
+                    </div>
+                    <div class="row">
+                        <a target="_blank" href="https://www.linkedin.com/in/andy-j-chen/"><i class="fa fa-linkedin"></i> https://www.linkedin.com/in/andy-j-chen/</a>
                     </div>
                 </div>
             </div>
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -13,136 +13,81 @@ const Portfolio = () =>{
 
     return(
         <div class="container ">
-        
-          
             <div class="col"> 
-            
             <h1>Portfolio</h1>
-        <div class="projects row">
-        <div class="col-md-4 col-sm-6 site">  
-            <Card >
-                <img class="img" src={img1} alt=""/>
-                <Card.Content>
-                    <Card.Header>Quokka</Card.Header>
-                    <Card.Description>Social Gardening App</Card.Description>
-                </Card.Content>
-                <a href="https://quokka-social-gardening.herokuapp.com/"><Button>Live Site</Button></a>
-                <a href="https://github.com/maxlemieux/quokka"><Button>Github Code</Button></a>  
-            </Card>
-        </div>
+                <div class="projects row">
+                    <div class="col-md-4 col-sm-6 site">  
+                        <Card >
+                            <img class="img" src={img1} alt=""/>
+                            <Card.Content>
+                                <Card.Header>Quokka</Card.Header>
+                                <Card.Description>Social Gardening App</Card.Description>
+                            </Card.Content>
+                            <a href="https://quokka-social-gardening.herokuapp.com/"><Button>Live Site</Button></a>
+                            <a href="https://github.com/maxlemieux/quokka"><Button>Github Code</Button></a>  
+                        </Card>
+                    </div>
 
-        <div class="col-md-4 col-sm-6 site" >  
-            <Card >
-                <img class="img" src={img2} alt=""/>
-                <Card.Content>
-                    <Card.Header>Weather App</Card.Header>
-                    <Card.Description>Check out weather in your city</Card.Description>
-                </Card.Content>
-                <a href="https://andyjchen24.github.io/WeatherAPI/"><Button>Live Site</Button></a>
-                <a href="https://github.com/AndyJChen24/WeatherAPI"><Button>Github Code</Button></a>   
-            </Card>
-        </div>
+                    <div class="col-md-4 col-sm-6 site" >  
+                        <Card >
+                            <img class="img" src={img2} alt=""/>
+                            <Card.Content>
+                                <Card.Header>Weather App</Card.Header>
+                                <Card.Description>Check out weather in your city</Card.Description>
+                            </Card.Content>
+                            <a href="https://andyjchen24.github.io/WeatherAPI/"><Button>Live Site</Button></a>
+                            <a href="https://github.com/AndyJChen24/WeatherAPI"><Button>Github Code</Button></a>   
+                        </Card>
+                    </div>
 
-        <div class="col-md-4 col-sm-6 site" > 
-            <Card>
-                <img class="img" src={img3} alt=""/>
-                <Card.Content>
-                    <Card.Header>WorkDayScheduler</Card.Header>
-                    <Card.Description>Plan your day</Card.Description>
-                </Card.Content>
-                <a href="https://andyjchen24.github.io/WorkDayScheduler/"><Button>Live Site</Button></a>
-                <a href="https://github.com/AndyJChen24/WorkDayScheduler"><Button>Github Code</Button></a>   
-            </Card>
-        </div>
+                    <div class="col-md-4 col-sm-6 site" > 
+                        <Card>
+                            <img class="img" src={img3} alt=""/>
+                            <Card.Content>
+                                <Card.Header>WorkDayScheduler</Card.Header>
+                                <Card.Description>Plan your day</Card.Description>
+                            </Card.Content>
+                            <a href="https://andyjchen24.github.io/WorkDayScheduler/"><Button>Live Site</Button></a>
+                            <a href="https://github.com/AndyJChen24/WorkDayScheduler"><Button>Github Code</Button></a>   
+                        </Card>
+                    </div>
 
-        <div class="col-md-4 col-sm-6 site" > 
-            <Card>
-                <img class="img" src={img4} alt=""/>
-                <Card.Content>
-                    <Card.Header>Workout Tracker</Card.Header>
-                    <Card.Description>Track your workout each day and duration</Card.Description>
-                </Card.Content>
-                <a href="https://fast-waters-35393.herokuapp.com/"><Button>Live Site</Button></a>
-                <a href="https://github.com/AndyJChen24/Workout-Tracker"><Button>Github Code</Button></a>   
-            </Card>
-        </div>
+                    <div class="col-md-4 col-sm-6 site" > 
+                        <Card>
+                            <img class="img" src={img4} alt=""/>
+                            <Card.Content>
+                                <Card.Header>Workout Tracker</Card.Header>
+                                <Card.Description>Track your workout each day and duration</Card.Description>
+                            </Card.Content>
+                            <a href="https://fast-waters-35393.herokuapp.com/"><Button>Live Site</Button></a>
+                            <a href="https://github.com/AndyJChen24/Workout-Tracker"><Button>Github Code</Button></a>   
+                        </Card>
+                    </div>
 
-        <div class="col-md-4 col-sm-6 site" > 
-            <Card>
-                <img class="img" src={img5} alt=""/>
-                <Card.Content>
-                    <Card.Header>Cangaroo</Card.Header>
-                    <Card.Description>Food Bank Donation System</Card.Description>
-                </Card.Content>
-                <a href="https://cangaroo.herokuapp.com/"><Button>Live Site</Button></a>
-                <a href="https://github.com/cwalker226/Cangaroo"><Button>Github Code</Button></a> 
-            </Card>
-        </div>
+                    <div class="col-md-4 col-sm-6 site" > 
+                        <Card>
+                            <img class="img" src={img5} alt=""/>
+                            <Card.Content>
+                                <Card.Header>Cangaroo</Card.Header>
+                                <Card.Description>Food Bank Donation System</Card.Description>
+                            </Card.Content>
+                            <a href="https://cangaroo.herokuapp.com/"><Button>Live Site</Button></a>
+                            <a href="https://github.com/cwalker226/Cangaroo"><Button>Github Code</Button></a> 
+                        </Card>
+                    </div>
 
-        <div class="col-md-4 col-sm-6 site" > 
-            <Card>
-                <img class="img" src={img6} alt=""/>
-                <Card.Content>
-                    <Card.Header>Notetaker</Card.Header>
-                    <Card.Description>An app to track your notes</Card.Description>
-                </Card.Content>
-                <a href="https://enigmatic-eyrie-77722.herokuapp.com/"><Button>Live Site</Button></a>
-                <a href="hhttps://github.com/AndyJChen24/Notetaker"><Button>Github Code</Button></a> 
-            </Card>
-        </div>
-        </div>
-            {/* <section class="projects row">
-              
-                <div class="col-md-4 col-sm-12 site" >
-                    <div class="imageContainer">
-                        <a href="https://quokka-social-gardening.herokuapp.com/"><img class="img" src={img1} alt=""/></a>
-                    </div>
-                    <div class="caption">
-                        <h5>Quokka-Social Gardening App</h5>
-                    </div>
-                    
-                </div>
-                <div class="col-md-4 col-sm-12 site" >
-                    <div class="imageContainer">
-                        <a href="https://andyjchen24.github.io/WeatherAPI/"><img class="img" src={img2} alt=""/></a>
-                    </div>
-                    <div class="caption">
-                        <h5>Weather API</h5>
+                    <div class="col-md-4 col-sm-6 site" > 
+                        <Card>
+                            <img class="img" src={img6} alt=""/>
+                            <Card.Content>
+                                <Card.Header>Notetaker</Card.Header>
+                                <Card.Description>An app to track your notes</Card.Description>
+                            </Card.Content>
+                            <a href="https://enigmatic-eyrie-77722.herokuapp.com/"><Button>Live Site</Button></a>
+                            <a href="hhttps://github.com/AndyJChen24/Notetaker"><Button>Github Code</Button></a> 
+                        </Card>
                     </div>
                 </div>
-                <div class="col-md-4 col-sm-12 site" >
-                    <div class="imageContainer">
-                        <a href="https://andyjchen24.github.io/WorkDayScheduler/"><img class="img" src={img3} alt=""/></a>
-                    </div>
-                    <div class="caption">
-                        <h5>Work Day Scheduler</h5>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12 site" >
-                    <div class="imageContainer">
-                        <a href="https://fast-waters-35393.herokuapp.com/"><img class="img" src={img4} alt=""/></a>
-                    </div>
-                    <div class="caption">
-                        <h5 >Workout Tracker</h5>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12 site" >
-                    <div class="imageContainer">
-                        <a href="https://cangaroo.herokuapp.com/"><img class="img" src={img5} alt=""/></a>
-                    </div>
-                    <div class="caption">
-                        <h5 >Cangaroo A Food Bank Application</h5>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-12 site" >
-                    <div class="imageContainer">
-                        <a href="https://enigmatic-eyrie-77722.herokuapp.com/"><img class="img" src={img6} alt=""/></a>
-                    </div>
-                    <div class="caption">
-                        <h5 >Notetaker</h5>
-                    </div>
-                </div>
-            </section> */}
             </div>
         </div>
     

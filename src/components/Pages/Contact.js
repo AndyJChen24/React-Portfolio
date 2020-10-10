0ff6eb53336a5d333f57ace34e1e936db6f33787
Andy <ajiajiechen@gmail.com>
2020-10-10 13:46:26 -0700

    fixed css for contact page

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -6,17 +6,17 @@ const Contact = () =>{
         <div class="container">
             <div class="row">
         
-                <div class="col col-md-12 col-sm-12 col-xs-12 col-lg-8">
+                <div class="col col-md-12 col-sm-12 col-xs-12 col-lg-12 col-xl-12">
                     <h1>Contact</h1>
                     <div class="row"><h6>Contact or Follow Me Here!</h6></div>
                     <div class="row">
                         <a href="mailto:andychen24@hotmail.com"><i class="fa fa-envelope"></i> andychen24@hotmail.com</a>
                     </div>
                     <div class="row">
-                        <a target="_blank" href="https://github.com/AndyJChen24"><i class="fa fa-github"></i> https://github.com/AndyJChen24</a>
+                        <a href="https://github.com/AndyJChen24"><i class="fa fa-github"></i> https://github.com/AndyJChen24</a>
                     </div>
                     <div class="row">
-                        <a target="_blank" href="https://www.linkedin.com/in/andy-j-chen/"><i class="fa fa-linkedin"></i> https://www.linkedin.com/in/andy-j-chen/</a>
+                        <a href="https://www.linkedin.com/in/andy-j-chen/"><i class="fa fa-linkedin"></i> https://www.linkedin.com/in/andy-j-chen/</a>
                     </div>
                 </div>
             </div>
